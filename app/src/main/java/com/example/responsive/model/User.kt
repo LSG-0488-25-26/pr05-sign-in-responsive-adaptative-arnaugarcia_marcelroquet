f35acc0055c7303860b9f94a32e778def8e02282
Marcel Roquet <marcel.roquet@gracia.lasalle.cat>
2025-12-27 11:27:04 +0100

    feat: user atributes

--- app/src/main/java/com/example/responsive/model/User.kt
+++ app/src/main/java/com/example/responsive/model/User.kt
@@ -1,4 +1,12 @@
 package com.example.responsive.model
 
-class User {
-}
+data class User(
+    val id: String = "",
+    val fullName: String = "",
+    val birthDate: String = "",
+    val email: String = "",
+    val phone: String = "",
+    val username: String = "",
+    val password: String = "",
+    val acceptTerms: Boolean = false
+)

6e1690b891985a696ac75e95f5f9a943e154f659
Marcel Roquet <marcel.roquet@gracia.lasalle.cat>
2025-12-27 13:00:22 +0100

    feat: register validations

--- app/src/main/java/com/example/responsive/viewModel/AppModel.kt
+++ app/src/main/java/com/example/responsive/viewModel/AppModel.kt
@@ -1,4 +1,37 @@
 package com.example.responsive.viewModel
 
-class AppModel {
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+
+class AppModel : ViewModel() {
+    // camps del fomrulari
+    val fullName = mutableStateOf("")
+    val birthDate = mutableStateOf("")
+    val email = mutableStateOf("")
+    val phone = mutableStateOf("")
+    val username = mutableStateOf("")
+    val password = mutableStateOf("")
+    val confirmPassword = mutableStateOf("")
+    val acceptTerms = mutableStateOf(false)
+
+    // missatge d'error
+    val errorMessage = mutableStateOf("")
+
+    // validació
+    fun validateRegister(): String {
+        errorMessage.value = when {
+            fullName.value.isEmpty() -> "Name required"
+            birthDate.value.isEmpty() -> "birthDate required"
+            email.value.isEmpty() || !email.value.contains("@") -> "invalid email"
+            phone.value.any { !it.isDigit() && it != ' ' && it != '+' } -> "only accept numbers"
+            username.value.isEmpty() -> "userName required"
+            password.value.length < 6 -> "password required 6 digits"
+            password.value != confirmPassword.value -> "Passwords do not match."
+            !acceptTerms.value -> "You must accept terms and conditions."
+            else -> ""
+        }
+        return errorMessage.value
+    }
+
+
 }
